4501270f5e6a7a5e8570844eca17c21018c9c58e
Kalle Svensson <knallle.morrum@gmail.com>
2018-07-04 20:15:54 +0200

    Fixed typos in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,10 +7,10 @@ export default ({data}) => (
       Kalle is a curious and tech savvy engineer who enjoy trying out new things.
     </p>
     <p>
-      This web site is the result of a few weeks worth of self studies in the fiel of modern web development. After a few weeks of consuming tutorials, blog posts and other media arguing for this or that framework, Kalle chose to settle on React.js and Gatsby.
+      This web site is the result of a few weeks worth of self studies in the field of modern web development. After a few weeks of consuming tutorials, blog posts and other media arguing for this or that framework, Kalle chose to settle on React.js and Gatsby.
     </p>
     <p>
-      The main structure of this site comes from an excellent tutorial at Gatsby's website (<a href="https://www.gatsbyjs.org/tutorial/">link</a>), which is stronly recommended for those who wish to do as Kalle did and learn how to develop a static web site.
+      The main structure of this site comes from an excellent tutorial at Gatsby's website (<a href="https://www.gatsbyjs.org/tutorial/">link</a>), which is strongly recommended for those who wish to do as Kalle did and learn how to develop a static web site.
     </p>
     <p>
       If you wish to learn more about Kalle and/or get in touch with him, please refer to the contact links at the bottom of the page.
---
+++ src/posts/hosting-a-website.md
@@ -0,0 +1,10 @@
+---
+title: "Hosting a site online"
+date: "2017-08-10"
+author: 'Kalle'
+type: 'Post'
+tags: ["gatsby", "web", "git"]
+published: false
+---
+
+A new post about hosting.
