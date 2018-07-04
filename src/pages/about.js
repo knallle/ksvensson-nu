5c2e00684a46893cb8c30b729a5e8a39fae52895
Kalle Svensson <knallle.morrum@gmail.com>
2018-07-04 22:45:08 +0200

    Updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,10 +7,10 @@ export default ({data}) => (
       Kalle is a curious and tech savvy engineer who enjoy trying out new things.
     </p>
     <p>
-      This web site is the result of a few weeks worth of self studies in the field of modern web development. After a few weeks of consuming tutorials, blog posts and other media arguing for this or that framework, Kalle chose to settle on React.js and Gatsby.
+      This web site is the result of a few weeks worth of self studies in the field of modern web development. Its purpose is twofold: i) by creating the site, Kalle got to learn a lot about modern web development and ii) the site will be a natural place for Kalle to document thoughts, projects, and discoveries.
     </p>
     <p>
-      The main structure of this site comes from an excellent tutorial at Gatsby's website (<a href="https://www.gatsbyjs.org/tutorial/">link</a>), which is strongly recommended for those who wish to do as Kalle did and learn how to develop a static web site.
+      The main structure of the site comes from an excellent tutorial at Gatsby's website (<a href="https://www.gatsbyjs.org/tutorial/">link</a>), which is strongly recommended for those who wish to do as Kalle did and learn how to develop a static web site.
     </p>
     <p>
       If you wish to learn more about Kalle and/or get in touch with him, please refer to the contact links at the bottom of the page.
